ab39b6e2b618a140c274df13af7a44214f05f149
krishika5 <sonikrishika555@gmail.com>
2025-08-26 11:50:37 +0530

    Add files via upload

---
+++ 22.08.25/22.aug.sql
@@ -0,0 +1,11 @@
+
+use students;
+alter table ece add(
+semail varchar(25), sphone int8);
+
+alter table ece drop column semail;
+ alter table ece add(
+ scountry varchar(40) default 'India'
+ );
+ select * from ece;
+ select sid, sname, sphone, scountry from ece;
